3a1cf69cdb1e2518c8887fb5001789b0542a0ac4
vitklimin <vitklimin@bk.ru>
2025-06-29 22:33:31 +0400

    The Main class has been modified to calculate text length, and a MANIFEST.MF file has been created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        String text = new Scanner(System.in) .nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
