ccc70da47faece1355f10192b50c390d08e11da5
vitklimin <vitklimin@bk.ru>
2025-06-30 14:10:37 +0400

    added text outputÐ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in) .nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите текст и нажмите Enter: ");
+        String text = scanner.nextLine();
         System.out.println("Длина текста: " + text.length());
     }
 }
