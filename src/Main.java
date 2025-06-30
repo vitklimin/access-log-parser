77b82813c3217751ec9f0c31de1c45b0068e3873
vitklimin <vitklimin@bk.ru>
2025-07-01 00:38:41 +0400

    We changed the code in the Main class, entered two numbers in the console, and received the result of the sum, difference, product, and division of the numbersÐ

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите текст и нажмите Enter: ");
-        String text = scanner.nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
     }
 }
